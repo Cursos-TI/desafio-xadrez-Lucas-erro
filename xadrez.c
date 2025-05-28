769f86d68718bdf8f3f7dc249ef4dd59a53e09cb
Lucas-erro <cristiancaue36@gmail.com>
2025-05-28 22:09:45 +0000

    criei o movimento da torre

--- xadrez.c
+++ xadrez.c
@@ -2,6 +2,13 @@
 
 int main() {
     printf("Xaderez Novato!\n");
+
+    // Movimento da Torre: 5 casas para a direita
+    int movimentoTorre = 5;  // Quantidade de casas que a Torre vai se mover
+    printf("Movimento da Torre:\n");
+    for(int i = 1; i <= movimentoTorre; i++) {
+        // A cada iteração, representa uma casa movida
+        printf("Direita\n");
     
     return 0;
 }
